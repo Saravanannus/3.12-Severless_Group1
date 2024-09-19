816b660239c335f515ee2d93799f5abe15d348b5
Saravanan.B <saravanannus@gmail.com>
2024-09-19 21:55:42 +0800

    update yml, and handler

--- handler.js
+++ handler.js
@@ -1,23 +1,42 @@
-// SNS Handler
-module.exports.snsHandler = async (event) => {
-  console.log("SNS event received:", JSON.stringify(event, null, 2));
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "SNS message processed successfully!",
-      input: event,
-    }),
-  };
-};
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
 
-// HTTP API Handler
-module.exports.httpHandler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from HTTP API!",
-      input: event,
-    }),
+module.exports.sendMessage = async (event) => {
+  let body;
+  
+  try {
+    // body = JSON.parse(event.body); // Parsing the request body
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'Invalid JSON format',
+      }),
+    };
+  }
+
+  const params = {
+    QueueUrl: process.env.SQS_QUEUE_URL,
+    MessageBody: "JSON.stringify(body)",
   };
+
+  try {
+    const data = await sqs.sendMessage(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Message sent to SQS',
+        messageId: data.MessageId
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: 'Failed to send message to SQS',
+      }),
+    };
+  }
 };
